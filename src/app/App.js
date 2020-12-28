15c0d6c47aa27ba0885b66330d85e60b41278b1d
Jakub Szajna <j.szajna@ideo.pl>
2020-12-28 22:31:58 +0100

    Problem with api connection

--- src/app/App.js
+++ src/app/App.js
@@ -11,3 +11,53 @@ export const App = ({ options }) => {
   testErrorReport()
   provider.get("wtf")
 }
+
+// Moje próby bez eksportu
+
+// class SportApiService {
+//   constructor(){
+//     API_KEY = process.env.SPORT_API_KEY
+//     API_LINK = `https://app.sportdataapi.com/api/v1/soccer/seasons?apikey=${API_KEY}`
+//   }
+  
+//   async getAllTeams(){
+//     const res = await this.get(`${this.creds.API_LINK}&league_id=314`)
+//     return res
+//   }
+  
+//   async getTeamByName(teamName){
+//      const res = await this.get(`${this.creds.API_LINK}/teams?name=${teamName}`)
+//      return res
+//   }
+// }
+
+// var myTeams = new SportApiService();
+// myTeams.getAllTeams();
+
+
+// Przykład z git'a
+
+// export default class SportApiService extends ApiService {
+//   constructor(){
+//      super({
+//         API_KEY = process.env.SPORT_API_KEY,
+//         API_LINK = `https://app.sportdataapi.com/api/v1/soccer/seasons?apikey=${API_KEY}`
+//      })
+//   }
+  
+//   async getAllTeams(){
+//      const res = await this.get(`${this.creds.API_LINK}&league_id=314`)
+//      return res
+//   }
+  
+//   async getTeamByName(teamName){
+//      const res = await this.get(`${this.creds.API_LINK}/teams?name=${teamName}`)
+//      return res
+//   }
+// }
+
+// var myTeams = new SportApiService();
+// myTeams.getAllTeams();
+
+const sportApi = provider.get("SportApi");
+sportApi.getTeamByName();
