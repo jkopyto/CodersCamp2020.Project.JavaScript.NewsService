dd2b7f20288ddee1a678973b80e16f125387fe7f
Kuba <k.kopyto@cypher.dog>
2020-12-29 17:19:05 +0100

    Fix

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,25 @@
 import provider from "../services/Provider"
 
-export const App = ({ options }) => {
-  const testErrorReport = () => {
-    provider.get("errorReporting").report({
-      error: new Error("Coś się popsuło i nie było mnie słychać"),
-      errorInfo: `It is a brand new error ${options}`,
-    })
-  }
-
-  testErrorReport()
-  provider.get("wtf")
-}
-
 class IndexView {
   constructor() {
-    window.addEventListener("haschange", (event) => this.onRouteChane(event))
+    window.onhashchange = (event) => this.onRouteChange(event)
   }
+
   onRouteChange(event) {
     console.log(event)
   }
 }
 
-new IndexView()
+export const App = ({ options }) => {
+  // const testErrorReport = () => {
+  //   provider.get("errorReporting").report({
+  //     error: new Error("Coś się popsuło i nie było mnie słychać"),
+  //     errorInfo: `It is a brand new error ${options}`,
+  //   })
+  // }
+
+  // testErrorReport()
+  // provider.get("wtf")
+
+  new IndexView()
+}
