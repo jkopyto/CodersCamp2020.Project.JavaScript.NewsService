2b1d691bd92000bb2bda775cee376b8391292e19
Kuba <k.kopyto@cypher.dog>
2020-12-30 10:23:23 +0100

    Router: fetching pages from src

--- src/services/Router.js
+++ src/services/Router.js
@@ -1,3 +1,5 @@
+import sportSubpage from "../subpages/sportSubpage/sportdata.html"
+
 export class Router {
   constructor() {
     window.addEventListener("hashchange", (event) => this.onRouteChange(event))
@@ -10,11 +12,25 @@ export class Router {
     this.loadContent(hashLocation)
   }
 
-  loadContent(uri) {
-    const contentUri = `/static/partials/${uri}.html`
-    fetch(contentUri)
-      .then((res) => res.text())
-      .then((content) => this.updateSlot(content))
+  async loadContent(uri) {
+    let content
+
+    switch (uri) {
+      case "sportdata":
+        content = sportSubpage
+        break
+      default:
+        content = await fetch(`/static/partials/${uri}.html`).then((res) =>
+          res.text()
+        )
+    }
+
+    // // const contentUri = `/static/partials/${uri}.html`
+    // fetch(contentUri)
+    //   .then((res) => res.text())
+    //   .then((content) => this.updateSlot(content))
+
+    this.updateSlot(content)
   }
 
   updateSlot(content) {
