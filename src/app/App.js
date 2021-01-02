023299efaeaf9eaf37443044ffd911bee2329fd7
adam-kostuch <adamkostuch.krakow@gmail.com>
2021-01-02 23:08:49 +0100

    Working connection

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import provider from "../services/Provider"
 
-export const App = async ({ options }) => {
+export const App = async () => {
   const newsApi = provider.get("NewsApi")
   const res = await newsApi.getWorldNews()
-  console.log(res)
+  return res
 }
--- src/services/NewsApiService/NewsApiService.js
+++ src/services/NewsApiService/NewsApiService.js
@@ -2,29 +2,30 @@ import ApiService from "../ApiService"
 
 export default class NewsApiService extends ApiService {
   constructor() {
-    super({
-      API_KEY: process.env.NEWS_API_KEY,
-      API_LINK: `https://newsapi.org/v2/everything?apikey=${process.env.NEWS_API_KEY}`,
-      API_BASE_LINK: `https://newsapi.org/v2/everything`,
-    })
+    super(
+      {
+        API_KEY: process.env.NEWS_API_KEY,
+        API_LINK: `https://newsapi.org/v2/everything?apikey=${process.env.NEWS_API_KEY}&pageSize=5`,
+      },
+      { headers: { Accept: "application/json" } }
+    )
   }
 
   async getWorldNews() {
     const res = await this.get(`${this.creds.API_LINK}&q=*`)
-    let data = await res.json()
-    console.log(this.creds.API_KEY)
+    const data = await res.json()
     return data
   }
 
   async getPolandNews() {
     const res = await this.get(`${this.creds.API_LINK}&language=pl&q=*`)
-    let data = await res.json()
+    const data = await res.json()
     return data
   }
 
   async getHealthNews() {
     const res = await this.get(`${this.creds.API_LINK}&q=health`)
-    let data = await res.json()
+    const data = await res.json()
     return data
   }
 }
