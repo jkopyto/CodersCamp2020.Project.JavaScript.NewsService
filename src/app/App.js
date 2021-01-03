6992c1873f1e1972e7b1bd3798bcbcbb772c0e43
Kacper <kacperzolkiewski@interia.pl>
2021-01-03 22:50:56 +0100

    connecting with Api

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,9 @@
 import provider from "../services/Provider"
 
-export const App = ({ options }) => {
-  const testErrorReport = () => {
-    provider.get("errorReporting").report({
-      error: new Error("Coś się popsuło i nie było mnie słychać"),
-      errorInfo: `It is a brand new error ${options}`,
-    })
-  }
-
-  testErrorReport()
-  provider.get("wtf")
+export const App = async () => {
+  const foodService = provider.get("FoodApi")
+  foodService.findProductByQuery("pizza")
+  foodService.getRandomRecipe()
+  foodService.getWinePairing("banana")
+  foodService.getIngredientDetails()
 }
--- src/services/FoodApiService/FoodApiService.js
+++ src/services/FoodApiService/FoodApiService.js
@@ -1,4 +1,5 @@
 import ApiService from "../ApiService"
+import process from "process"
 
 export default class FoodApiService extends ApiService {
   constructor() {
@@ -6,22 +7,42 @@ export default class FoodApiService extends ApiService {
       {
         API_KEY: process.env.FOOD_API_KEY,
         API_LINK: `https://api.spoonacular.com/api_key=${process.env.FOOD_API_KEY}`,
+        API_BASE_LINK: "https://api.spoonacular.com",
       },
       { headers: { Accept: "application/json" } }
     )
   }
 
-
-  async getRecipesByNutriens(){
-    const res = await this.get(`${this.creds.API_LINK}/recipes/findByNutrients`)
-    let data = await res.json()
+  async findProductByQuery(query) {
+    const res = await this.get(
+      `${this.creds.API_BASE_LINK}/food/products/search?query=${query}&number=10&apiKey=${this.creds.API_KEY}`
+    )
+    const data = await res.json()
     return data
   }
 
-  async findByIngredients(){
-    const res = await this.get(`${this.creds.API_LINK}/recipes/findByIngredients`)
-    let data = await res.json()
+  async getRandomRecipe() {
+    const res = await this.get(
+      `${this.creds.API_BASE_LINK}/recipes/random?apiKey=${this.creds.API_KEY}`
+    )
+    const data = await res.json()
     return data
   }
 
+  async getWinePairing(food) {
+    const res = await this.get(
+      `${this.creds.API_BASE_LINK}/food/wine/pairing?food=${food}&apiKey=${this.creds.API_KEY}`
+    )
+    const data = await res.json()
+    return data
+  }
+
+  async getIngredientDetails() {
+    const res = await this.get(
+      `${this.creds.API_BASE_LINK}/food/ingredients/9266/information?amount=1&apiKey=${this.creds.API_KEY}`
+    )
+    const data = await res.json()
+    console.log(data)
+    return data
+  }
 }
