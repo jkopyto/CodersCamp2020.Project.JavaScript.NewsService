bc3bb19d6a0232999d0077e1bb3ce25c253bfd6d
Kacper <kacperzolkiewski@interia.pl>
2021-01-03 22:50:56 +0100

    connecting with Api

--- src/app/App.js
+++ src/app/App.js
@@ -1,1 +1,9 @@
-export const App = () => {}
+import provider from "../services/Provider"
+
+export const App = async () => {
+  const foodService = provider.get("FoodApi")
+  foodService.findProductByQuery("pizza")
+  foodService.getRandomRecipe()
+  foodService.getWinePairing("banana")
+  foodService.getIngredientDetails()
+}
