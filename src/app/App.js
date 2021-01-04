01997ee8c7139a2ccc8bc8f5ea68281ca7f9abf1
Kacper <kacperzolkiewski@interia.pl>
2021-01-04 21:43:55 +0100

    update service

--- src/app/App.js
+++ src/app/App.js
@@ -2,8 +2,6 @@ import provider from "../services/Provider"
 
 export const App = async () => {
   const foodService = provider.get("FoodApi")
-  foodService.findProductByQuery("pizza")
-  foodService.getRandomRecipe()
-  foodService.getWinePairing("banana")
-  foodService.getIngredientDetails()
+
+  foodService.getRecipeByCalories(200)
 }
--- src/services/FoodApiService/FoodApiService.js
+++ src/services/FoodApiService/FoodApiService.js
@@ -16,33 +16,40 @@ export default class FoodApiService extends ApiService {
   async findProductByQuery(query) {
     const res = await this.get(
       `${this.creds.API_BASE_LINK}/food/products/search?query=${query}&number=10&apiKey=${this.creds.API_KEY}`
-    )
-    const data = await res.json()
-    return data
+    ).then((res) => res.json())
+
+    return res
   }
 
   async getRandomRecipe() {
     const res = await this.get(
       `${this.creds.API_BASE_LINK}/recipes/random?apiKey=${this.creds.API_KEY}`
-    )
-    const data = await res.json()
-    return data
+    ).then((res) => res.json())
+
+    return res
   }
 
   async getWinePairing(food) {
     const res = await this.get(
       `${this.creds.API_BASE_LINK}/food/wine/pairing?food=${food}&apiKey=${this.creds.API_KEY}`
-    )
-    const data = await res.json()
-    return data
+    ).then((res) => res.json())
+
+    return res
   }
 
   async getIngredientDetails() {
     const res = await this.get(
       `${this.creds.API_BASE_LINK}/food/ingredients/9266/information?amount=1&apiKey=${this.creds.API_KEY}`
-    )
-    const data = await res.json()
-    console.log(data)
-    return data
+    ).then((res) => res.json())
+
+    return res
+  }
+
+  async getRecipeByCalories(calories) {
+    const res = await this.get(
+      `${this.creds.API_BASE_LINK}/recipes/findByNutrients?minCalories=50&maxCalories=${calories}&number=5&apiKey=${this.creds.API_KEY}`
+    ).then((res) => res.json())
+
+    return res
   }
 }
