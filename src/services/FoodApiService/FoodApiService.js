09cf3390f48c9e59bdbc0a968526c3fc455710e2
Kacper <kacperzolkiewski@interia.pl>
2021-01-06 15:57:16 +0100

    Creating Tests

--- src/services/FoodApiService/FoodApiService.js
+++ src/services/FoodApiService/FoodApiService.js
@@ -40,10 +40,14 @@ export default class FoodApiService extends ApiService {
   async getIngredientDetailsById(id) {
     const res = await this.get(
 <<<<<<< HEAD
+<<<<<<< HEAD
       `${this.creds.API_BASE_LINK}/food/ingredients/${id}/information?amount=1&apiKey=${this.creds.API_KEY}`
 =======
       `${this.creds.API_BASE_LINK}/food/ingredients/9266/information?amount=1&apiKey=${this.creds.API_KEY}`
 >>>>>>> 393fe96 (update service)
+=======
+      `${this.creds.API_BASE_LINK}/food/ingredients/${id}/information?amount=1&apiKey=${this.creds.API_KEY}`
+>>>>>>> 392b3f7 (Creating Tests)
     ).then((res) => res.json())
 
     return res
@@ -74,5 +78,21 @@ export default class FoodApiService extends ApiService {
     return res
 =======
 >>>>>>> 393fe96 (update service)
+  }
+
+  async getRecipeInformationById(id) {
+    const res = await this.get(
+      `${this.creds.API_BASE_LINK}/recipes/${id}/information?includeNutrition=false&apiKey=${this.creds.API_KEY}`
+    ).then((res) => res.json())
+
+    return res
+  }
+
+  async getWineDescription(wine) {
+    const res = await this.get(
+      `${this.creds.API_BASE_LINK}/food/wine/description?wine=${wine}&apiKey=${this.creds.API_KEY}`
+    ).then((res) => res.json())
+
+    return res
   }
 }
