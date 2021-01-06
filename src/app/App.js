90ab928559488416bcbc979fa5edf622a109b3f5
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-01-06 13:09:04 +0100

    Remove unused code

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,9 @@
 import provider from "../services/Provider"
 
 export const App = async () => {
-  const currencyAPI = provider.get("CurrencyAPI")
-  const allCoins = await currencyAPI.getAllCoins()
-  const singleCoin = await currencyAPI.getSingleCoin("btc-bitcoin")
-  const exchangeCoinToUSD = await currencyAPI.exchangeCoinToUSD("btc-bitcoin", 17)
+  }
 
-  console.log(allCoins)
-  console.log(singleCoin)
-  console.log(exchangeCoinToUSD)
-  // console.log(allCoins)
-  // console.log(singleCoin)
-  // return allCoins
-}
+
+
+
+
