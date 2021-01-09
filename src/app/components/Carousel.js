d3b5911f691f7d5d4440f2c7473dd24ea4e48082
Rafał Litwinowicz <rafal.litwinowicz@gmail.com>
2021-01-09 16:47:21 +0100

    added carousel config, slide images, created homepage to contain carousel

--- src/app/components/Carousel.js
+++ src/app/components/Carousel.js
@@ -1,8 +1,19 @@
+const config = {
+  type: "carousel",
+  startAt: 0,
+  perView: 2,
+  breakpoints: {
+    600: {
+      perView: 1,
+    },
+  },
+}
+
 import Glide from "@glidejs/glide"
 
 export class CarouselComponent {
   constructor(selector) {
-    this.glide = new Glide(selector)
+    this.glide = new Glide(selector, config)
   }
 
   init() {
