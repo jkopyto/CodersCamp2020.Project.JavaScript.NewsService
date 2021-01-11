c6093e0b4fa2c6bfa7d171a7ab90b0a3ff4e9b0d
Rafał Litwinowicz <rafal.litwinowicz@gmail.com>
2021-01-11 20:47:57 +0100

    some scripts moved from index.js to App.js, window.onload removed

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,31 @@
 import provider from "../services/Provider"
+import { SentryReporting, ConsoleReporting } from "../services/ErrorReporting"
+import AppResourceProvider from "../services/ResourceProvider/AppResourceProvider"
+import FoodApiService from "../services/FoodApiService"
+import FetchClient from "../services/Http"
+import Router from "../services/Router"
+import CryptoCurrencyService from "../services/CryptoCurrencyService/CryptoCurrency.service"
+import WeatherApiService from "../services/WeatherService"
+
+const registerDependencyFunc = () => {
+  if (process.env.SENTRY_DSN) {
+    provider.provide(
+      "errorReporting",
+      new SentryReporting(process.env.SENTRY_DSN, process.env.GIT_COMMIT)
+    )
+  } else {
+    provider.provide("errorReporting", new ConsoleReporting())
+  }
+  provider.provide("resourceProvider", new AppResourceProvider())
+  provider.provide("httpClient", new FetchClient({ maxRetries: 2 }))
+  provider.provide("router", new Router())
+  provider.provide("weatherApiService", new WeatherApiService())
+  provider.provide("CurrencyAPI", new CryptoCurrencyService())
+  provider.provide("FoodApi", new FoodApiService())
+}
 
 export const App = () => {
+  registerDependencyFunc()
   const router = provider.get("router")
   router.onRouteChange()
 }
