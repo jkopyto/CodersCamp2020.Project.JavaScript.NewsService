f1bb71aa9de513218288c1799b2472fc4432e732
R4fau <60842382+R4fau@users.noreply.github.com>
2021-01-12 19:10:19 +0100

    Refreshbugfix (#31)
    * working, responisve topbar ready, active subpage not distinguished
    
    * some scripts moved from index.js to App.js, window.onload removed

--- src/index.js
+++ src/index.js
@@ -1,28 +1,4 @@
 import "regenerator-runtime/runtime"
 import { App } from "./app/App"
-import provider from "./services/Provider"
-import { SentryReporting, ConsoleReporting } from "./services/ErrorReporting"
-import AppResourceProvider from "./services/ResourceProvider/AppResourceProvider"
-import FoodApiService from "./services/FoodApiService"
-import FetchClient from "./services/Http"
-import Router from "./services/Router"
-import CryptoCurrencyService from "./services/CryptoCurrencyService/CryptoCurrency.service"
-import WeatherApiService from "./services/WeatherService"
 
-if (process.env.SENTRY_DSN) {
-  provider.provide(
-    "errorReporting",
-    new SentryReporting(process.env.SENTRY_DSN, process.env.GIT_COMMIT)
-  )
-} else {
-  provider.provide("errorReporting", new ConsoleReporting())
-}
-
-provider.provide("resourceProvider", new AppResourceProvider())
-provider.provide("httpClient", new FetchClient({ maxRetries: 2 }))
-provider.provide("router", new Router())
-provider.provide("weatherApiService", new WeatherApiService())
-provider.provide("CurrencyAPI", new CryptoCurrencyService())
-provider.provide("FoodApi", new FoodApiService())
-
-window.onload = () => App()
+App()
