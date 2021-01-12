e056daca91d4047e8c352aa2920a06c97cab09c8
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-01-12 23:42:56 +0100

    Add pagination and input search

--- src/subpages/cryptocurrencySubpage/CryptoCurrency.js
+++ src/subpages/cryptocurrencySubpage/CryptoCurrency.js
@@ -6,6 +6,7 @@ export default class CryptoCurrencySubpage {
 
   _allCoins = []
   _coinsToRender = []
+  pageSize = 500
   coinsList = document.querySelector("#coins-list")
 
 
@@ -14,13 +15,62 @@ export default class CryptoCurrencySubpage {
     this.init()
   }
 
+  coinsToRender = () => {
+    const nextPageBtn = document.querySelector("#next-page")
+    const prevPageBtn = document.querySelector("#prev-page")
+    const inputSearch = document.querySelector("#search-coin")
 
+    let actualPage = 1
+
+    nextPageBtn.addEventListener("click", () => {
+      actualPage++
+      const startPageFrom = actualPage * this.pageSize
+      const finishPageAt = actualPage * this.pageSize + this.pageSize
+      this._coinsToRender = this._allCoins.slice(startPageFrom, finishPageAt)
+      this.renderPage()
+    })
+
+    prevPageBtn.addEventListener("click", () => {
+      actualPage--
+      const startPageFrom = actualPage * this.pageSize
+      const finishPageAt = actualPage * this.pageSize + this.pageSize
+      this._coinsToRender = this._allCoins.slice(startPageFrom, finishPageAt)
+      this.renderPage()
+    })
+
+
+    inputSearch.addEventListener("keyup", (e) => {
+      if (e.target.value.length === 0) {
+        this._coinsToRender = this._allCoins.slice(0, this.pageSize)
+        actualPage = 1
+        this.renderPage()
+      } else {
+        this._coinsToRender = this._allCoins.filter(coin => coin.name.toLowerCase().includes(e.target.value.toLowerCase())
+        )
+      }
+      this.renderPage()
+    })
+
+  }
+
+  renderPage = () => {
+    this.coinsList.innerHTML = null
+    this._coinsToRender.forEach(
+      coin => {
+        this.coinsList.innerHTML += `<p data-id="${coin.id}">
+                                     ${coin.name}
+                                </p>`
+      }
+    )
+  }
 
 
   init = async () => {
     await this._currencyAPI.getAllCoins()
       .then(r => this._allCoins = [...r])
-      .then(r => this._coinsToRender = r.slice(0, 500))
+      .then(r => this._coinsToRender = r.slice(0, 5))
+    await this.renderPage()
+    await this.coinsToRender()
   }
 
 
