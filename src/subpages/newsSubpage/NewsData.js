cf84818491868cec8d25b101e64516614b6eab6b
adam-kostuch <adamkostuch.krakow@gmail.com>
2021-01-17 18:08:30 +0100

    Making transition between array elements

--- src/subpages/newsSubpage/NewsData.js
+++ src/subpages/newsSubpage/NewsData.js
@@ -1,7 +1,6 @@
 import provider from "../../services/Provider"
 import Subpage from "../Subpage"
 import css from "./NewsSubpage.css"
-import "./NewsSubpage.css"
 
 export default class NewsSubpage extends Subpage {
   constructor() {
@@ -9,40 +8,58 @@ export default class NewsSubpage extends Subpage {
     this._newsApi = provider.get("NewsApiService")
   }
 
-  getNewsContentDiv() {
-    return document.querySelector(".news__container")
+  getNewsContentDiv1() {
+    return document.querySelector(".news__content1")
+  }
+
+  getNewsContentDiv2() {
+    return document.querySelector(".news__content2")
   }
 
   generateMessage(news, output, i) {
     output[i] = []
     news.articles.forEach((e) => {
       output[i].push(
-        this.generateDiv(e.title, e.author, e.content.substring(0, 200))
+        this.generateDiv(e.title, e.author, e.content.substring(0, 200), e.url)
       )
     })
     return output
   }
 
-  generateDiv(title, author, content) {
+  generateDiv(title, author, content, link) {
     return `
-      <div>
-        <h5>${title}</h5>
-        <h6>${author}</h6>
-        <p>${content}</p>
-      </div>
+      <a href='${link}'>
+        <div>
+          <h5>${title}</h5>
+          <h6>${author}</h6>
+          <p>${content}</p>
+        </div>
+      </a>
     `
   }
 
-  generateOutput(array, index = 0) {
-    let output = ""
-    let newIndex = index
+  incrementIndex = (index) => {
+    let newIndex = index + 1
     if (newIndex == 5) newIndex = 0
-    array.forEach((innerArray) => {
-      output += innerArray[newIndex]
-    })
+    return newIndex
+  }
 
-    this.getNewsContentDiv().innerHTML = output
-    setTimeout(this.generateOutput, 5000, array, ++newIndex)
+  changeOpacity1 = (array, index) => {
+    const newIndex = this.incrementIndex(index)
+    this.getNewsContentDiv1().innerHTML =
+      array[0][newIndex] + array[1][newIndex] + array[2][newIndex]
+    this.getNewsContentDiv2().style.opacity = 0
+    this.getNewsContentDiv1().style.opacity = 1
+    setTimeout(this.changeOpacity2, 5000, array, newIndex)
+  }
+
+  changeOpacity2 = (array, index) => {
+    const newIndex = this.incrementIndex(index)
+    this.getNewsContentDiv2().innerHTML =
+      array[0][newIndex] + array[1][newIndex] + array[2][newIndex]
+    this.getNewsContentDiv2().style.opacity = 1
+    this.getNewsContentDiv1().style.opacity = 0
+    setTimeout(this.changeOpacity1, 5000, array, newIndex)
   }
 
   async updatePage() {
@@ -67,7 +84,7 @@ export default class NewsSubpage extends Subpage {
           (outputArray = this.generateMessage(res, outputArray, arrayIndex++))
       )
 
-    this.generateOutput(outputArray, 0)
+    this.changeOpacity1(outputArray, 0)
   }
 
   async render() {
