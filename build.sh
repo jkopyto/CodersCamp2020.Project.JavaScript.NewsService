ad1219c15f2fd6841f0ec11fc817c3bda88f6e1b
Kuba <k.kopyto@cypher.dog>
2021-01-17 18:03:36 +0100

    cleanup

--- build.sh
+++
@@ -1,7 +0,0 @@
-commit=$(git rev-parse --short=8 HEAD)
-export GIT_COMMIT="$commit"
-export SENTRY_DSN="https://e6839d71823a4f06af00108cf7dcd6c9@o485829.ingest.sentry.io/5541712"
-
-npm i -g parcel
-npm install
-npm run build
