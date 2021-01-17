b23860cfe27bb0797d57afaa4ad84de95833146c
Jakub Kopyto <50829078+jkopyto@users.noreply.github.com>
2021-01-17 18:05:30 +0100

    Migrate to github workflow (#43)
    * test
    
    * testv2
    
    * testv3
    
    * testv4
    
    * testv5
    
    * testv6
    
    * testv7
    
    * testv8
    
    * testv9
    
    * testv10
    
    * testv11
    
    * testv12
    
    * testv13
    
    * testv14
    
    * testv15
    
    * testv16
    
    * testv17
    
    * testv18
    
    * testv19
    
    * testv20
    
    * testv21
    
    * testv22
    
    * testv23
    
    * testv24
    
    * testv25
    
    * testv26
    
    * testfix
    
    * testfixv2
    
    * testfixv3
    
    * cleanup
    
    Co-authored-by: Kuba <k.kopyto@cypher.dog>

--- src/app/components/Topbar.js
+++ src/app/components/Topbar.js
@@ -1,9 +1,9 @@
 export default class Topbar {
   constructor() {}
   setActiveClass(subpageID) {
-    const elements = document.getElementsByClassName("active")   
+    const elements = document.getElementsByClassName("active")
     for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.remove("active")
+      elements[i].classList.remove("active")
     }
     document.getElementById(subpageID).classList.add("active")
   }
