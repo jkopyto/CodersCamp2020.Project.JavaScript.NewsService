663b7bf7d39b422ce269d8a0e9525271a4f2c344
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-01-18 16:44:15 +0100

    Code format refactor

--- src/subpages/cryptocurrencySubpage/CryptoCurrency.js
+++ src/subpages/cryptocurrencySubpage/CryptoCurrency.js
@@ -3,7 +3,6 @@ import css from "./CryptoCurrency.css"
 import Subpage from "../Subpage"
 
 export default class CryptoCurrencySubpage extends Subpage {
-
   _allCoins = []
   _coinsToRender = []
   _actuallyDisplayedCoinsId = []
@@ -38,27 +37,29 @@ export default class CryptoCurrencySubpage extends Subpage {
       this.renderPage()
     })
 
-
     inputSearch.addEventListener("keyup", (e) => {
       if (e.target.value.length === 0) {
         this._coinsToRender = this._allCoins.slice(0, this.pageSize)
         actualPage = 0
         this.renderPage()
       } else {
-        this._coinsToRender = this._allCoins.filter(coin => coin.name.toLowerCase().includes(e.target.value.toLowerCase())
+        this._coinsToRender = this._allCoins.filter((coin) =>
+          coin.name.toLowerCase().includes(e.target.value.toLowerCase())
         )
       }
       this.renderPage()
     })
   }
 
-
   openModal = async () => {
     const closeModal = document.querySelector(".modal__close")
 
-    await this._actuallyDisplayedCoinsId.forEach(coin => coin.addEventListener("click", () => {
+    await this._actuallyDisplayedCoinsId.forEach((coin) =>
+      coin.addEventListener("click", () => {
         this._coinId = coin.getAttribute("data-coinId")
-        this._currencyAPI.getSingleCoin(this._coinId).then(r => this.renderModal(r))
+        this._currencyAPI
+          .getSingleCoin(this._coinId)
+          .then((r) => this.renderModal(r))
         this.modalWindow.classList.add("modal--open")
       })
     )
@@ -70,24 +71,28 @@ export default class CryptoCurrencySubpage extends Subpage {
   exchangeCoins = (e) => {
     e.preventDefault()
     const moneySpan = document.querySelector("span.modal__money")
-    const coinsInputValue = document.querySelector("form.modal__form").coins.value
-    this._currencyAPI.exchangeCoinToUSD(this._coinId, coinsInputValue).then(r => parseFloat(r).toFixed(2)).then(r => moneySpan.textContent = r)
+    const coinsInputValue = document.querySelector("form.modal__form").coins
+      .value
+    this._currencyAPI
+      .exchangeCoinToUSD(this._coinId, coinsInputValue)
+      .then((r) => parseFloat(r).toFixed(2))
+      .then((r) => (moneySpan.textContent = r))
   }
 
   exchangeCoinToUSD = async (coinId, amountOfCoins) => {
-    await this._currencyAPI.exchangeCoinToUSD(coinId, amountOfCoins).then(r => r)
+    await this._currencyAPI
+      .exchangeCoinToUSD(coinId, amountOfCoins)
+      .then((r) => r)
   }
 
   renderPage = () => {
     this.coinsList.innerHTML = null
-    this._coinsToRender.forEach(
-      coin => {
-        this.coinsList.innerHTML += `<p
+    this._coinsToRender.forEach((coin) => {
+      this.coinsList.innerHTML += `<p
  data-coinId="${coin.id}" 
  class="coins-list__coin">
 ${coin.name}</p>`
-      }
-    )
+    })
     this.updateActuallyCoinId()
   }
 
@@ -102,25 +107,27 @@ ${coin.name}</p>`
   }
 
   updateActuallyCoinId = () => {
-    this._actuallyDisplayedCoinsId = [...document.querySelectorAll("[data-coinId]")]
+    this._actuallyDisplayedCoinsId = [
+      ...document.querySelectorAll("[data-coinId]"),
+    ]
     this.openModal()
   }
 
-
   catchHTMLElements = () => {
-    this.modalForm = document.querySelector("form.modal__form").addEventListener("submit", (e) => this.exchangeCoins(e))
+    this.modalForm = document
+      .querySelector("form.modal__form")
+      .addEventListener("submit", (e) => this.exchangeCoins(e))
     this.coinsList = document.querySelector("#coins-list")
     this.modalWindow = document.querySelector(".modal")
   }
 
   render = async () => {
     await this.catchHTMLElements()
-    await this._currencyAPI.getAllCoins()
-      .then(r => this._allCoins = [...r])
-      .then(r => this._coinsToRender = r.slice(0, this.pageSize))
+    await this._currencyAPI
+      .getAllCoins()
+      .then((r) => (this._allCoins = [...r]))
+      .then((r) => (this._coinsToRender = r.slice(0, this.pageSize)))
     await this.renderPage()
     await this.coinsToRender()
   }
-
-
 }
