07778a20b48caf49f3aba50b6403166d349dea7d
Mateusz Cabała <49129082+mateuszCabala95@users.noreply.github.com>
2021-01-18 19:43:21 +0100

    Feature/crypto currency page (#55)
    * Create branch CryptoCurrencyPage
    
    Add init method
    
    * Add pagination and input search
    
    * Add modal functionality
    
    * Update modal
    
    Add exchange form in modal
    
    * Add exchange form
    
    * Add style for input search
    
    * Extends Subpage
    
    * Code refactor
    
    * Add style for mobile devices
    
    * Add style for tablet and desktop
    
    * Code format refactor
    
    * Fix issue
    
    * fix modal bug
    
    * Code refactor
    
    Move modal code to ModalView.js
    
    * format refactor
    
    * Update CryptoCurrency.js

--- src/subpages/cryptocurrencySubpage/CryptoCurrency.js
+++ src/subpages/cryptocurrencySubpage/CryptoCurrency.js
@@ -1,18 +1,18 @@
 import provider from "../../services/Provider"
 import css from "./CryptoCurrency.css"
 import Subpage from "../Subpage"
+import ModalView from "./ModalView"
 
 export default class CryptoCurrencySubpage extends Subpage {
-
   _allCoins = []
   _coinsToRender = []
   _actuallyDisplayedCoinsId = []
-  _coinId
-  pageSize = 200
+  pageSize = 80
 
   constructor() {
     super(css)
     this._currencyAPI = provider.get("CurrencyAPI")
+    this.modal = new ModalView()
   }
 
   coinsToRender = () => {
@@ -38,89 +38,53 @@ export default class CryptoCurrencySubpage extends Subpage {
       this.renderPage()
     })
 
-
     inputSearch.addEventListener("keyup", (e) => {
       if (e.target.value.length === 0) {
         this._coinsToRender = this._allCoins.slice(0, this.pageSize)
         actualPage = 0
         this.renderPage()
       } else {
-        this._coinsToRender = this._allCoins.filter(coin => coin.name.toLowerCase().includes(e.target.value.toLowerCase())
+        this._coinsToRender = this._allCoins.filter((coin) =>
+          coin.name.toLowerCase().includes(e.target.value.toLowerCase())
         )
       }
       this.renderPage()
     })
   }
 
-
-  openModal = async () => {
-    const closeModal = document.querySelector(".modal__close")
-
-    await this._actuallyDisplayedCoinsId.forEach(coin => coin.addEventListener("click", () => {
-        this._coinId = coin.getAttribute("data-coinId")
-        this._currencyAPI.getSingleCoin(this._coinId).then(r => this.renderModal(r))
-        this.modalWindow.classList.add("modal--open")
-      })
-    )
-    closeModal.addEventListener("click", () => {
-      this.modalWindow.classList.remove("modal--open")
-    })
-  }
-
-  exchangeCoins = (e) => {
-    e.preventDefault()
-    const moneySpan = document.querySelector("span.modal__money")
-    const coinsInputValue = document.querySelector("form.modal__form").coins.value
-    this._currencyAPI.exchangeCoinToUSD(this._coinId, coinsInputValue).then(r => parseFloat(r).toFixed(2)).then(r => moneySpan.textContent = r)
-  }
-
-  exchangeCoinToUSD = async (coinId, amountOfCoins) => {
-    await this._currencyAPI.exchangeCoinToUSD(coinId, amountOfCoins).then(r => r)
-  }
-
   renderPage = () => {
     this.coinsList.innerHTML = null
-    this._coinsToRender.forEach(
-      coin => {
-        this.coinsList.innerHTML += `<p
+    this._coinsToRender.forEach((coin) => {
+      this.coinsList.innerHTML += `<p
  data-coinId="${coin.id}" 
- class="coins-list__coin">
+ class="coins-list__coin button offer-button">
 ${coin.name}</p>`
-      }
-    )
+    })
     this.updateActuallyCoinId()
   }
 
-  renderModal = (clickedCoin) => {
-    const coinTitle = document.querySelector("h3.modal__title")
-    const coinSymbol = document.querySelector("span.modal__symbol")
-    const coinDescription = document.querySelector("p.modal__description")
-
-    coinTitle.textContent = clickedCoin.name
-    coinSymbol.textContent = clickedCoin.symbol
-    coinDescription.textContent = clickedCoin.description
-  }
-
   updateActuallyCoinId = () => {
-    this._actuallyDisplayedCoinsId = [...document.querySelectorAll("[data-coinId]")]
-    this.openModal()
+    this._actuallyDisplayedCoinsId = [
+      ...document.querySelectorAll("[data-coinId]"),
+    ]
+    this.modal.openModal(this._actuallyDisplayedCoinsId, this.modalWindow)
   }
-
 
   catchHTMLElements = () => {
-    this.modalForm = document.querySelector("form.modal__form").addEventListener("submit", (e) => this.exchangeCoins(e))
+    this.modalForm = document
+      .querySelector("form.modal__form")
+      .addEventListener("submit", (e) => this.modal.exchangeCoins(e))
     this.coinsList = document.querySelector("#coins-list")
     this.modalWindow = document.querySelector(".modal")
   }
 
   render = async () => {
     await this.catchHTMLElements()
-    await this._currencyAPI.getAllCoins()
-      .then(r => this._allCoins = [...r])
-      .then(r => this._coinsToRender = r.slice(0, this.pageSize))
+    await this._currencyAPI
+      .getAllCoins()
+      .then((r) => (this._allCoins = [...r]))
+      .then((r) => (this._coinsToRender = r.slice(0, this.pageSize)))
     await this.renderPage()
     await this.coinsToRender()
   }
-
-
 }
