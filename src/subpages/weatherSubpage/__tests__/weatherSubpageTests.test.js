67d50d2f7f41d9b2ec8517d529d09b479eb761c4
Karolina Gutka <74671633+kgutka@users.noreply.github.com>
2021-01-18 23:55:15 +0100

    Feature/forecast weather chart (#54)
    * Add chart prototype
    
    * Fix bug with coords, style canvas
    
    * Format time X axis, create getAirPollution method
    
    * Feature/mainpage (#42)
    
    * added calendar widget and carousel autoplay
    
    * selected active subpage on topbar
    
    * active subpage selection in topbar no-deselect-bug-fix
    
    * added copyright footer box
    
    * Add air pollution div
    
    * Fix bug in test
    
    * Change names due to naming convention
    
    * Feature/crypto currency page (#55)
    
    * Create branch CryptoCurrencyPage
    
    Add init method
    
    * Add pagination and input search
    
    * Add modal functionality
    
    * Update modal
    
    Add exchange form in modal
    
    * Add exchange form
    
    * Add style for input search
    
    * Extends Subpage
    
    * Code refactor
    
    * Add style for mobile devices
    
    * Add style for tablet and desktop
    
    * Code format refactor
    
    * Fix issue
    
    * fix modal bug
    
    * Code refactor
    
    Move modal code to ModalView.js
    
    * format refactor
    
    * Update CryptoCurrency.js
    
    * Feature/food subpage css (#57)
    
    * Css changes
    
    * Finishing css sybpage styles
    
    * Style subpage based on proposed style
    
    * Fix issue with Prettier
    
    Co-authored-by: R4fau <60842382+R4fau@users.noreply.github.com>
    Co-authored-by: Mateusz Cabała <49129082+mateuszCabala95@users.noreply.github.com>
    Co-authored-by: kacperzolkiewski <74975508+kacperzolkiewski@users.noreply.github.com>

--- src/subpages/weatherSubpage/__tests__/weatherSubpageTests.test.js
+++ src/subpages/weatherSubpage/__tests__/weatherSubpageTests.test.js
@@ -1,13 +1,11 @@
-import WeatherData from "../WeatherData"
-
-const weatherData = new WeatherData()
+import { getDateAndDayOfWeek } from "../weatherUtils"
 
 describe("Get proper date tests", () => {
   it("Returns a valid date", () => {
     const mockDate = new Date(2021, 0, 14, 12)
     const spy = jest.spyOn(global, "Date").mockImplementation(() => mockDate)
 
-    const [date, dayOfWeek] = weatherData.getDateAndDayOfWeek(21)
+    const [date, dayOfWeek] = getDateAndDayOfWeek(21)
 
     spy.mockRestore()
 
