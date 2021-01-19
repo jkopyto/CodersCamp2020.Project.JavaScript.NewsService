c475eac183446ca5adf9f3c938f7c7c9bd519407
Jakub Szajna <kubaszajna@gmail.com>
2021-01-19 16:32:16 +0100

    Tests + favicon + title

--- src/services/SportApiService/__tests__/mockResponses.js
+++ src/services/SportApiService/__tests__/mockResponses.js
@@ -64,6 +64,42 @@ const matchResponse = {
   },
 }
 
+const leagueResponse = {
+  team_id: 4798,
+  position: 1,
+  points: 43,
+  status: "Promotion",
+  result: "Champions League",
+  overall: {
+    games_played: 18,
+    won: 13,
+    draw: 4,
+    lost: 1,
+    goals_diff: 20,
+    goals_scored: 39,
+    goals_against: 19,
+  },
+  home: {
+    games_played: 9,
+    won: 5,
+    draw: 3,
+    lost: 1,
+    goals_diff: 8,
+    goals_scored: 20,
+    goals_against: 12,
+  },
+  away: {
+    games_played: 9,
+    won: 8,
+    draw: 1,
+    lost: 0,
+    goals_diff: 12,
+    goals_scored: 19,
+    goals_against: 7,
+  },
+}
+
 module.exports = {
   matchResponse,
+  leagueResponse,
 }
